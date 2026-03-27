dc44ba91fa8ab146416d376c8423a368cdfc1a92
srinad2323 <larafun42@gmail.com>
2026-03-27 19:38:25 -0400

    Add files via upload

---
+++ task2.ino
@@ -0,0 +1,91 @@
+/*
+  Blink without Delay
+
+  Turns on and off a light emitting diode (LED) connected to a digital pin,
+  without using the delay() function. This means that other code can run at the
+  same time without being interrupted by the LED code.
+
+  The circuit:
+  - Use the onboard LED.
+  - Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA
+    and ZERO it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN
+    is set to the correct LED pin independent of which board is used.
+    If you want to know what pin the on-board LED is connected to on your
+    Arduino model, check the Technical Specs of your board at:
+    https://docs.arduino.cc/hardware/
+
+  created 2005
+  by David A. Mellis
+  modified 8 Feb 2010
+  by Paul Stoffregen
+  modified 11 Nov 2013
+  by Scott Fitzgerald
+  modified 9 Jan 2017
+  by Arturo Guadalupi
+
+  This example code is in the public domain.
+
+  https://docs.arduino.cc/built-in-examples/digital/BlinkWithoutDelay/
+*/
+
+// constants won't change. Used here to set a pin number:
+const int ledPin = LED_BUILTIN;  // the number of the LED pin
+
+// Variables will change:
+int ledState1 = LOW;  // ledState used to set the LED
+int ledState2 = LOW;
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis1 = 0;  // will store last time LED was updated
+unsigned long previousMillis2 = 0;
+
+// constants won't change:
+const long interval1 = 500;  // interval at which to blink (milliseconds)
+const long interval2 = 2000;
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis1 >= interval1) {
+    // save the last time you blinked the LED
+    previousMillis1 = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState1 == LOW) {
+      ledState1 = HIGH;
+    } else {
+      ledState1 = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(12, ledState1);
+
+  }
+
+  if (currentMillis - previousMillis2 >= interval2) {
+    // save the last time you blinked the LED
+    previousMillis2 = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState2 == LOW) {
+      ledState2 = HIGH;
+    } else {
+      ledState2 = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(8, ledState2);
+  }
+}
